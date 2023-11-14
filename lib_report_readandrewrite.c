eb515cc815fa46d547dc6646b55030bf3d90d208
Atifa111 <139042240+Atifa111@users.noreply.github.com>
2023-11-14 19:17:18 +0700

    v4

--- lib_report_readandrewrite.c
+++ lib_report_readandrewrite.c
@@ -13,63 +13,57 @@ typedef struct
     char mb[11];//member
 } report_info ;
 
-int read_file_report(FILE **fp, report_info *sheet);
+void read_file_report(FILE **fp, report_info *sheet, int *indexs);
 
-int read_file_report(FILE **fp, report_info *sheet)
+void read_file_report(FILE **fp, report_info *sheet, int *indexs)
 {
 
     *fp = fopen("report_sheet.csv","r+");
 
-    int index;
     float num;
     char tmp[200];
 
     fscanf( *fp, "%s", tmp);
-    printf("%s", tmp);
 
-    for (int i = 0; fscanf( *fp, "%s", tmp) != EOF; i++)
+    for (int i = 0; fscanf( *fp, "%s", tmp) != EOF; i++, *indexs = i)
     {
 
         char *token = strtok( tmp, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->lp, token);
 
         token = strtok( NULL, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->pv, token);
 
         token = strtok( NULL, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->t_in, token);
 
         token = strtok( NULL, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->t_out, token);
 
         token = strtok( NULL, ",");
         num = atoi(token);
-        printf( "\n token %.2f", token);
         (sheet + i)->price = num;
 
         token = strtok( NULL, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->cl, token);
         
         token = strtok( NULL, ",");
-        printf( "\n token %s", token);
         strcpy( (sheet + i)->mb, token);
-        (sheet + i)->mb[10] = '\0';
+        (sheet + i)->mb[11] = '\0';
 
-        printf( "\n license_plate %d : %s"  , i, (sheet + i)->lp);
-        printf( "\n province      %d : %s"  , i, (sheet + i)->pv);
-        printf( "\n time_in       %d : %s"  , i, (sheet + i)->t_in);
-        printf( "\n time_out      %d : %s"  , i, (sheet + i)->t_out);
-        printf( "\n price         %d : %.2f", i, (sheet + i)->price);
-        printf( "\n car_location  %d : %s"  , i, (sheet + i)->cl);
-        printf( "\n member        %d : %s"  , i, (sheet + i)->mb);
     }
-        
+    
+    printf("\nindex is %d", *indexs);
     fclose(*fp);
+}
+
+void input_lp(int *index)
+{
+    char n_lp[7];
+    char id_pv[3];
+
+    printf("license plate : ");
+    scanf("%6s%2s", n_lp, id_pv);
 }
 
 int main()
@@ -78,6 +72,8 @@ int main()
     FILE *file;
 
     report_info sheets[100];
+    int index;
 
-    read_file_report( &file, sheets);
+    read_file_report( &file, sheets, &index);
+    input_lp(&index);
 }
