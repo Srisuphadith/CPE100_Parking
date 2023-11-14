33b1b90e8c6c482994b61a15ff2eb17a6ad83dde
Nine <ninerattana2005@gmail.com>
2023-11-14 13:47:52 +0700

    debug

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include "lib_car_location.c"
 int main() // A(1-8),B(9-16),C(17,24)
 {
-    //location car[25];
-    //FILE *fp_location;
+    location car[25];
+    FILE *fp_location;
     //read_file_location(&fp_location, car);
-    //write_to_file_location(&fp_location, "A", 5, 1, "00791600");
+    write_to_file_location(&fp_location, "A", 1, 1, "00791600");
 
     return 0;
 }
