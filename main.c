416999934aa47ce9bbdad7e93ee7008e17832cef
Nine <ninerattana2005@gmail.com>
2023-11-14 13:50:36 +0700

    debug

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main() // A(1-8),B(9-16),C(17,24)
 {
     location car[25];
     FILE *fp_location;
-    read_file_location(&fp_location, car);
-    //write_to_file_location(&fp_location, "A", 5, 1, "00791600");
+    //read_file_location(&fp_location, car);
+    write_to_file_location(&fp_location, "A", 5, 1, "00791600");
 
     return 0;
 }
