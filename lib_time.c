44f5bb199a63ae69c034704b2f170aaca0fa765a
Atifa111 <139042240+Atifa111@users.noreply.github.com>
2023-11-16 02:41:59 +0700

    v15

--- lib_time.c
+++ lib_time.c
@@ -5,6 +5,7 @@
 
 int call_time(char *time_);
 void cv_itoc_time(int i_hr, int i_min, int i_sec, char *c_time);
+void cv_df_stotime(int t_sec, char *time);
 void df_ctoi_time(char *c_time1, char *c_time2, int *df_sec);
 
 void cv_itoc_time(int i_hr, int i_min, int i_sec, char *c_time)
@@ -20,30 +21,28 @@ void cv_itoc_time(int i_hr, int i_min, int i_sec, char *c_time)
     strcat( c_time, c_sec);
 }
 
+void cv_df_stotime(int t_sec, char *time)
+{
+    
+}
+
 void df_ctoi_time(char *c_time1, char *c_time2, int *df_sec)
 {
-    df_sec = 0;
+    int df = 0;
     char *token = strtok( c_time2, ":");
-        printf("%s\n", token);
-        printf("%d\n", atoi(token)*3600);
-        printf("%d\n", df_sec);
-        df_sec = df_sec + (atoi(token)*3600);
-        printf("%d\n", df_sec);
+        df += (atoi(token)*3600);
     token = strtok( NULL, ":");
-        df_sec += atoi(token)*60;
-        printf("%d\n", df_sec);
+        df += atoi(token)*60;
     token = strtok( NULL, ":");
-        df_sec += atoi(token);
-        printf("%d\n", df_sec);
+        df += atoi(token);
     token = strtok( c_time1, ":");
-        df_sec -= atoi(token)*3600;
-        printf("%d\n", df_sec);
+        df -= atoi(token)*3600;
     token = strtok( NULL, ":");
-        df_sec -= atoi(token)*60;
-        printf("%d\n", df_sec);
+        df -= atoi(token)*60;
     token = strtok( NULL, ":");
-        df_sec -= atoi(token);
-        printf("%d", df_sec);
+        df -= atoi(token);
+        printf("%d\n", df);
+    *df_sec = df;
 }   
 
 int call_time(char *time_)
@@ -61,10 +60,11 @@ int call_time(char *time_)
 int main()
 {
     char t[9];
-    char t2[9] = "2:30:00";
+    char t2[9] = "2:40:00";
     int dfsec;
 
     call_time(t);
     printf("%s\n", t);
     df_ctoi_time( t, t2, &dfsec);
+    printf("%d", dfsec);
 }
