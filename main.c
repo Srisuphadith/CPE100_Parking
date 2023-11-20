5d79efd254ed14ca662b842f9c47f69e5333107a
Nine <ninerattana2005@gmail.com>
2023-11-20 14:50:58 +0700

    v2

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include "lib_car_location.c"
+#include "convert_province.c"
+#include "print_summary.c"
+#include"lib_input_lp.c"
 int main() // A(1-8),B(9-16),C(17,24)
 {
-    location car[25];
-    FILE *fp_location;
-<<<<<<< Updated upstream
-    read_file_location(&fp_location, car);
-    //write_to_file_location(&fp_location, "A", 5, 1, "00791600");
-=======
-    //read_file_location(&fp_location, car);
-    write_to_file_location(&fp_location, "A", 5, 0, "AB791600");
->>>>>>> Stashed changes
+      //FILE *file;
+   // report_info report[100];
+    //int index = 0;
 
-    return 0;
+    //read_file_report( &file, report, &index);
+    //input_lp( &file, report, &index);
+    //location car[25];
+    //FILE *fp_location;
+    //read_file_location(&fp_location, car);
+    //write_to_file_location(&fp_location, "A", 5, 1, "00791600");
+    
+//feen
+    // int id_province;
+    // char name_province[25] = "";
+    // printf("Enter id of province : ");
+    // scanf(" %d", &id_province);
+    // convert_province(id_province,name_province); //convert_province(int *const , char *const)
+    // printf("%s\n", name_province);
+
+    // print_data("report_sheet.csv"); //print_data(File name);
 }
