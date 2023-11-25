177be3d8da64adc81a3c105073917bf19c027941
Feen0305 <pheen.phoorin@gmail.com>
2023-11-25 20:53:21 +0700

    MERGE 100%
    comment main

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main() // A(1-8),B(9-16),C(17,24)
     int options;
     float time_speed=1.00;
 
+    //Create a variable to store data from the input license plate function.
     FILE *file;
     report_info report[100];
     int index = 0;
@@ -27,19 +28,21 @@ int main() // A(1-8),B(9-16),C(17,24)
     while (1) {
         system("cls");
         read_file_report(&file, report, &index);
+        //read file report_sheet to collect a data of report and amount of car in report_sheet.
         while (input_lp(&file, report, &index, time_speed) != -1) {}
-        
+        //input_lp == -1 mean user enter "admin000".
         system("cls");
+        
         printf("Please, Verify your Identity\n");
         printf("----------------------------\n");
-        
+        //when user enter "admin000" , user need to enter a valid password.
         do {
             printf("Enter Password: ");
             scanf(" %s", verify_pass);
         } while (strcmp(verify_pass, base_pass) != 0);
         
+        //when user enter a vaild password ,program will go next to admin console.
         printf("Verify Success\n");
-
         do {
             printf("\nWelcome to Administrator Console.\n");
             printf("--------------------------------\n");
@@ -51,34 +54,42 @@ int main() // A(1-8),B(9-16),C(17,24)
             printf("6 : Find Location From License\n");
             printf("7 : Find License From Location\n");
             printf("0 : Back\n");
-            printf("Enter a number of Options: ");
+            //admin enter an options to continue.
+            printf("Enter a number of Options to continue: ");
             scanf(" %d", &options);
             printf("\n");
             system("cls");
 
+            //call function depend on options that admin enter.
             switch (options) {
                 case 0:
                     break;
                 case 1:
+                //call print data function to pritnt out all of data.
                     printf("Summary Overall Information.\n\n");
                     print_data("report_sheet.csv");
                     break;
                 case 2:
+                //call findAvailable function to pritnt out Avaible Parking Space.
                     printf("Avaible Parking Space --> %d\n", findAvailable(&fp_location, car, car_AVB));
                     break;
                 case 3:
-                    printf("Registor Member.\n\n");
+                //call registerV function to Register Member.
+                    printf("Register Member.\n\n");
                     registerV(NULL);
                     break;
                 case 4:
+                //call registerV function to pritnt out all of member.
                     printf("Member:\n");
                     read_member(NULL);
                     break;
                 case 5:
+                //call showGraphic function to pritnt out simulation of car in that floor.
                     printf("Simulation.\n\n");
                     showGraphic(NULL);
                     break;
                 case 6:
+                //call findLocationFromLicense function to pritnt out Location of that License plate.
                     char *result_locate = findLocationFromLicense(&fp_location, car);
                     if (strcmp(result_locate, "License not found") == 0)
                     {
@@ -91,6 +102,7 @@ int main() // A(1-8),B(9-16),C(17,24)
                     free(result_locate);
                     break;
                 case 7:
+                //call findLicenseFromLocation function to pritnt out License plate of that Location.
                     char *result_lisense = findLicenseFromLocation(&fp_location, car);
                     if (strcmp(result_lisense, "Doesn't Have This Location") == 0)
                     {
@@ -102,11 +114,15 @@ int main() // A(1-8),B(9-16),C(17,24)
                     }
                     break;
                 default:
+                //print error if user enter a invalid number options.
                     printf("Invalid option, please try again.\n");
             }
+        //if options == 0 break the loop and go back to input license plate console.
         } while (options != 0);
     }
     return 0;
+}
+
 
     // location car[25];
     // location car_AVB[25];
@@ -130,4 +146,3 @@ int main() // A(1-8),B(9-16),C(17,24)
     // registerV();
     // read_member();
     
-}
