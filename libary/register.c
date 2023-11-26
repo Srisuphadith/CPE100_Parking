56c3ad0ce79d24d64b26eb749e3894a527b53d98
NutNuttawut <128456226+AlphaMasterHub@users.noreply.github.com>
2023-11-26 23:11:35 +0700

    Update register.c
    add comment registerV()

--- libary/register.c
+++ libary/register.c
@@ -22,35 +22,35 @@ void read_member(){
 
 void check_member(char lp[],char* stat_member)
 {
-    FILE *fp = fopen( "member.csv", "r");
+    FILE *fp = fopen( "member.csv", "r");// Open the "member.csv" file for reading
 
-    if( fp == NULL)
+    if( fp == NULL)// Check if the file opening is successful
     {
         printf("Error opening member_file");
         return;
     }
 
-    char tmp[11];
-    while(fscanf( fp, "%s", tmp) != EOF)
+    char tmp[11];// Temporary variable to store license plates read from the file
+    while(fscanf( fp, "%s", tmp) != EOF)// Read license plates from the file until the end of the file (EOF) is reached
     {
-        if ( strcmp( lp, tmp) == 0)
+        if ( strcmp( lp, tmp) == 0)// Compare the given license plate (lp) with the one read from the file (tmp)
         {
-            strcpy( stat_member, "member");
+            strcpy( stat_member, "member");// If a match is found, set the status to "member" and return from the function
             return;
         }
     }
-    strcpy( stat_member, "non-member");
+    strcpy( stat_member, "non-member");// If the loop completes without finding a match, set the status to "non-member"
 
-    fclose(fp);
+    fclose(fp);// Close the file before returning
     return;
 }
 
 int registerV() {
-    printf("Register System\n");
+    printf("Register System\n");// Print a welcome message
     
-    while (1) {
+    while (1) {// Infinite loop for continuous user input
         char full_licens_plate[20];
-        printf("License plate (type 'END' to stop entry) : ");
+        printf("License plate (type 'END' to stop entry) : ");// Prompt the user to enter the license plate
         scanf(" %s", full_licens_plate);
         char n_lp[7];
         char id_pv[3];
@@ -58,34 +58,34 @@ int registerV() {
         
         //check a format of license plate-----------
         if (strlen(full_licens_plate)==8){
-            sscanf(full_licens_plate, "%6s%2s", n_lp, id_pv);
+            sscanf(full_licens_plate, "%6s%2s", n_lp, id_pv);// Extract the license plate components
         }
         else{
-            if (strcmp("END", full_licens_plate) == 0) 
+            if (strcmp("END", full_licens_plate) == 0)// If the format is invalid, check if the user entered 'END' to stop entry 
             {
-                break;  
+                break;// Exit the loop if 'END' is entered  
             }
             printf("License plate's format is invalid\n");
-            break;
+            break;// Exit the loop for any other invalid format
         }
-        if (convert_province( atoi(id_pv), pv) == 0 )
+        if (convert_province( atoi(id_pv), pv) == 0 )// Check if the province ID is valid using the convert_province function
         {
             printf("This id : %s not found as the province\n",id_pv);
-            break;
+            break;// Exit the loop if the province ID is not found
         }
         //-----------------------------------------------
 
-        FILE *fp = fopen("member.csv", "r");
+        FILE *fp = fopen("member.csv", "r");// Open the "member.csv" file for reading
         if (fp == NULL) 
         {
             printf("Error opening file.\n");
-            break;
+            break;// Exit the loop if the file cannot be opened
         }
 
         char data[9];
         int check = 0;
 
-        while (fscanf(fp, "%s", data) != EOF) {
+        while (fscanf(fp, "%s", data) != EOF) {// Check if the vehicle is already registered
             if (strcmp(full_licens_plate, data) == 0) 
             {
                 check = 1;
@@ -93,15 +93,15 @@ int registerV() {
             }
         }
 
-        fclose(fp);
+        fclose(fp);// Close the file
 
-        if (check == 0) 
+        if (check == 0)  // If the vehicle is not already registered, add it to the "member.csv" file
         {
             fp = fopen("member.csv", "a");
             if (fp == NULL) 
             {
                 printf("Error opening file.\n");
-                break; 
+                break; // Exit the loop if the file cannot be opened for writing
             }
             fprintf(fp, "%s\n", full_licens_plate);
             fclose(fp);
@@ -109,7 +109,7 @@ int registerV() {
         }
     }
 
-    return 0;
+    return 0;// Return 0 to indicate successful completion
 
 }
 
